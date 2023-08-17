eaaf5f39340bf32d80a6da8791457aa91a717e49
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-08-17 19:07:05 +0530

    backend start done

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
 
+//db connection
+// const connectDB = require("./config/db");
+// connectDB();
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+app.use(express.json());
+app.use(express.urlencoded());
+//define the port number
+const port = process.env.PORT || 5000;
+
+//start the server
+const server = app.listen(port, () =>
+  console.log(`Server running on port ${port} 🔥`)
+);
